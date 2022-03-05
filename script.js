c681af6e13a3755f157ce5d98f734ae80e4494db
Takeshi-ooiwa <98926777+Takeshi-ooiwa@users.noreply.github.com>
2022-03-06 00:15:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ cd_text = new Array("①", "②①", "③②①");
 
 color_text = "#000000";
 color_count = "#CCCCCC";
+//空白の回答対応
+let sss = "";
 
 function f_start() {
     if (flag == 0) {
@@ -21,6 +23,7 @@ function f_start() {
         // set[3] = document.flash.set4.value;
         //タイマー起動
         // timerID = setInterval('f_count()', set[2] * 1000);
+        document.querySelector('h4').innerText = "";
         count = 0;
         document.flash.b_start.disabled = true;
         kotae = 0;
@@ -38,13 +41,12 @@ function f_count() {
         f_clear();
     } else {
         //乱数の生成
-        var r = Math.random();
+        let r = Math.random();
         r = Math.floor(r * Math.pow(10, set[0]) * 0.9 + Math.pow(10, set[0] - 1));
         //数値の表示
         document.flash.number.style.color = color_text;
         document.flash.number.value = getComma(r);
         kotae += r;
-
         timerID = setTimeout('f_blank()', set[2] * 1000);
     }
 }
@@ -60,50 +62,39 @@ function f_clear() {
     clearInterval(timerID);
     // document.flash.number.value = "";
     document.flash.b_start.disabled = false;
-    document.flash.b_start.value = "解答表示";
+    document.flash.b_start.value = "答え合わせをする";
     document.flash.b_start.focus();
     flag = 1;
 }
 
-
 function f_kotae() {
-    var s = document.flash.number.value;
-    var t = "";
-    if (s != "") {
-        //答え合わせ
-        cnt2++;
-        if (s == kotae) {
-            // alert("正解です！！");
-            alert(" \nYay! Test PASSED. \n\n 正解です！！");
-
-            t = "○";
-            cnt1++;
-        } else {
-            alert("答えは" + getComma(kotae) + "でした。");
-            alert("    actual: ", s);
-            alert("  expected: ", getComma(kotae));
-            t = "×";
-        }
-    } else {
+    let s = document.flash.number.value;
+    sss = s;
+    let t = "";
+    //答え合わせ
+    cnt2++;
+    if (s === "") {
         //入力なし
         // alert("答えは" + getComma(kotae) + "です。");
-        alert("    actual: ", s);
-        alert("  expected: ", getComma(kotae));
-
-
-        // t = "×";
-
-
-
-
-
+        document.querySelector('h4').innerText = `actual:  ${s}\nexpected:  ${getComma(kotae)}\n\nNothing has been entered`;
+        t = "×";
+    } else if (s == kotae) {
+        // alert("正解です！！");
+        document.querySelector('h4').innerText = ` \nYay! Test PASSED. \n\n 正解です！！`;
+        t = "○";
+        cnt1++;
+    } else if (s !== kotae) {
+        // alert("答えは" + getComma(kotae) + "でした。");
+        document.querySelector('h4').innerText = `actual:  ${s}\nexpected:  ${getComma(kotae)}\n\nOh my God! Is Wrong, so again`;
+        t = "×";
     }
+
     //初期化
     document.flash.b_start.value = "スタート";
     flag = 0;
     document.flash.number.value = "";
     //成績の履歴
-    seiseki += " " + t + getComma(kotae);
+    seiseki += " " + t + getComma(kotae) + " ; ";
     if (cnt2 > 0) {
         t = " [" + cnt1 + "/" + cnt2 + "]  ";
     }
@@ -131,9 +122,12 @@ number_format();
 
 function number_format() {
     //初期化
-    document.flash.reset();
-    document.flash.number.value = "";
-    document.flash.number.style.color = color_text;
-    document.flash.b_start.disabled = false;
-    document.flash.b_start.focus();
+    if (sss === true) {
+        document.flash.number.value = "";
+        document.flash.number.style.color = color_text;
+        document.flash.b_start.disabled = false;
+        document.flash.reset();
+        document.flash.b_start.focus();
+    }
+
 }
